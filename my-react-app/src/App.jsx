707749aa195de7539ece202a49a7065b182243e5
PushToTalk <x_killer90@mail.ru>
2022-12-14 20:37:42 +0300

    Adding 3 components: Button, InputText and Main(App)

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from './components/Button/Button';
+import TextInput from './components/TextInput/TextInput';
+import './App.css';
+
+function App() {
+  return (
+    <form className="App">
+      <TextInput />
+      <Button />
+    </form>
+  );
+}
+
+export default App;
