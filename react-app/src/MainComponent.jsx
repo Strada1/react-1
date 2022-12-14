d87ef09b30366d9895d6ecbcdfedf2a5d1b51db6
Inal Lukyaev <lukyaevinal@gmail.com>
2022-12-14 22:10:30 +0300

    add task #3

---
+++ react-app/src/MainComponent.jsx
@@ -0,0 +1,10 @@
+import { Button } from './Button.jsx';
+import { TextInput } from './TextInput';
+export function MainComponent() {
+  return (
+    <div>
+      <TextInput></TextInput>
+      <Button></Button>
+    </div>
+  );
+}
