e168e66314de83f879f7a98808c451f68d073835
hollywood787 <hollywood7878@yandex.ru>
2022-12-14 18:29:37 +0300

    UI components for Genderize

---
+++ danis/src/components/Button.jsx
@@ -0,0 +1,5 @@
+function Button() {
+  return <button>Узнать пол</button>;
+}
+
+export default Button;
--- danis/src/main.jsx
+++ danis/src/main.jsx
@@ -1,53 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Button from "./components/Button";
+import Input from "./components/Input";
 
-function formatName(user) {
-    return (`${user.name} ${user.familia}`)
-}
-
-const user = {
-    name: 'Danis',
-    familia: 'Minnigalin'
-}
-
-function TitleHeader() {
-  return <h1>Привет, {formatName(user)}</h1>;
-}
-
-function WriteTask({border}) {
-  return <input style={{border}} type="text" placeholder="введи" />;
-}
-
-function TaskItem({ label, important = false }) {
-  const style = {
-    color: important ? "tomato" : "black",
-  };
-
-  return <li style={style}>{label}</li>;
-}
-
-function TaskItems() {
-  return (
-    <ul>
-      <TaskItem label="Drink Cofee" />
-      <TaskItem label="Create my First APP" important />
-    </ul>
-  );
-}
-
-function AddTask({color}) {
-  return <button style={{color}}>Click me!</button>;
-}
-
-function App() {
+function Main() {
   return (
     <div>
-      <TitleHeader name="Danis" />
-      <WriteTask border="1px solid yellow"/>
-      <TaskItems />
-      <AddTask  color="green" />
+      <form action="">
+        <Input />
+        <Button />
+      </form>
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Main />, document.getElementById("root"));
