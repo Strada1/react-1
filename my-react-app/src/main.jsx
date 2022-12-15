7d6857e3614755c80ba17fe75314a086e71a7d47
Dariannyko <dariannyko@gmail.com>
2022-12-15 20:59:06 +0300

    add handleClick on button

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,38 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Main } from "./app/App";
 
-function Main(props) {
-  return (
-    <div className={props.class}>
-      <form className="form">
-        <Input type="text" name="name" placeholder="Your name" />
-        <Button />
-      </form>
-    </div>
-  );
-}
-
-function Input(props) {
-  return (
-    <label>
-      <input
-        className="input"
-        type={props.type}
-        name={props.name}
-        placeholder={props.placeholder}
-      />
-    </label>
-  );
-}
-
-function Button() {
-  return (
-    <button className="button" type="submit">
-      Click me
-    </button>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Main class="container" />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Main class="container"/>
+  </React.StrictMode>
+);
