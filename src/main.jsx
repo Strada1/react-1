627827571b8ea6741470562d7ca1cc8682702cfd
codeinium <timofiei.tarasov@gmail.com>
2022-12-16 01:18:05 +0500

    work with event handlers

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Button from "./components/button/Button.jsx";
-import Input from "./components/input/Input.jsx";
-// import './main.css'
-
-const TEXTINBUTTON = "НАЖМИ";
+import { App } from "./components/App/App.jsx";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="App">
-      <Input> </Input>
-      <Button textInButton={TEXTINBUTTON}> </Button>
-    </div>
+    <App />
   </React.StrictMode>
 );
