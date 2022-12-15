d13a7560fe4892338d4b41ba02acda518e4e8bfb
Dima <61251772+DimaSU2020@users.noreply.github.com>
2022-12-15 15:57:34 +0300

    add input and button listeners

--- react-genderize/src/components/UiForm.jsx
+++ react-genderize/src/components/UiForm.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import { React } from 'react'
 import { MyButton } from './UI/button/MyButton'
 import { MyInput } from './UI/input/MyInput'
 
 const UiForm = () => {
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log('form work')
+  }
+  function handleInput (event) {
+    event.preventDefault()
+    console.log(event.target.value)
+  }
+  function handleButton () {
+    console.log('im here')
+  }
   return (
-    <form className='request'>
-      <MyInput className='input-request' type='text' placeholder=' First Name' autoFocus/>
-      <MyButton className='button-request'>Request</MyButton>
+    <form className='request' onSubmit={handleSubmit}>
+      <MyInput 
+        className='input-request'
+        type='text'
+        placeholder=' First Name' 
+        autoFocus
+        onChange={handleInput}
+      />
+      <MyButton 
+        className='button-request'
+        type='submit' 
+        onClick={handleButton}
+      >
+        Request
+      </MyButton>
     </form>
   )
 }
