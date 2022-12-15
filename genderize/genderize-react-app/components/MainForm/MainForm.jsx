b03b04fe5bb3b6327cc44ae649714cea4d8c8645
Varensev Vitaliy <vitalik.unit@gmail.com>
2022-12-15 18:46:23 +0500

    add  event listeners

---
+++ genderize/genderize-react-app/components/MainForm/MainForm.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import TextInput from "../TextInput/TextInput.jsx";
+import Button from "../Button/Button.jsx";
+import './MainForm.css'
+
+class MainForm extends React.Component {
+    request(e) {
+        e.preventDefault();
+        const value = document.querySelector(".input-name").value;
+        console.log("request sent " + value)
+    }
+
+    render() {
+        return (
+            <div>
+                <form className="main-form" onSubmit={this.request}>
+                    <TextInput />
+                    <Button/>
+                </form>
+            </div>
+        );
+    }
+
+}
+
+export default MainForm;
---
+++ genderize/genderize-react-app/components/TextInput/TextInput.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './TextInput.css';
+
+class TextInput extends React.Component {
+    handleChange() {
+        console.log(document.querySelector(".input-name").value)
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    className="input-name"
+                    type="text"
+                    placeholder="Введите имя"
+                    onInput={this.handleChange}/>
+            </div>
+        );
+    }
+}
+
+export default TextInput;
