9aff55548a8b73bb60000da84c92e9d42105c9ac
d33sf0me <katekrokh@gmail.com>
2022-12-16 20:02:08 +0300

    #3 Состояние и жизненный цикл

---
+++ d33sf0me/practice-3/src/App.jsx
@@ -0,0 +1,17 @@
+import TextInput from './components/input/input'
+import Button from './components/button/button'
+import Gender from './components/main/main'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <TextInput />
+      <Button />
+      <Gender />
+    </div>
+  )
+}
+
+export default App
