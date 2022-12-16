b87a42f84a03141b36eb493642b39bca8d74de6c
Alexey Rusakov <alexeyrusakov1999@gmail.com>
2022-12-16 23:48:31 +0300

    add EvList

--- my-react-app/my-app/src/Components/ButtonComp/Button.jsx
+++ my-react-app/my-app/src/Components/ButtonComp/Button.jsx
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 import "./btnStyles.css";
 
 export default class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.handlerClick = this.handlerClick.bind(this);
+  }
+
+  handlerClick(event) {
+    event.preventDefault();
+    console.log("Hello bitches");
+  }
+
   render() {
-    return <button className={this.props.className}>{this.props.text}</button>;
+    return (
+      <button className={this.props.className} onClick={this.handlerClick}>
+        {this.props.text}
+      </button>
+    );
   }
 }
