876792b2ab6fbae2d0172852dcd2372bd41a5b2a
Nikita <ifyoucan1337@yandex.ru>
2022-12-17 13:53:13 +0300

    fix

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export default function TextInput() {
-	return <input type="text" className="name" name="inputName" />;
+export default function TextInput({ value, setValue }) {
+	return (
+		<input
+			onChange={evt => setValue(evt.target.value)}
+			value={value}
+			type="text"
+			className="name"
+			name="inputName"
+		/>
+	);
 }
