8a9ea3a4f27f20f1865f68a1921bf895756fdd89
kamilsalikhov75 <kamilsalikhov75@gmail.com>
2022-12-17 23:04:25 +0300

    #5 Немного практики

---
+++ my-react-app/src/const.js
@@ -0,0 +1,8 @@
+export const url = 'https://api.genderize.io?name='
+
+export const classes = {
+    form: "gender__form",
+    input: "gender__input",
+    button: "gender__button",
+    activeButton: "gender__button--active"
+}
