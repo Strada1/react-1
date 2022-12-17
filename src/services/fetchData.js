bc3de44d5a4b9be4503beebba4464dacafc1568f
Dmitriy Bakhtin <dmitri.bakhtin@gmail.com>
2022-12-17 22:40:48 +0200

    refactoring

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,9 +1,18 @@
+const SERVER_URL = 'https://api.genderize.io';
+
 export async function fetchData(name) {
-	const SERVER_URL = 'https://api.genderize.io';
 	const url = `${SERVER_URL}?name=${name}`;
 
-	const response = await fetch(url);
-	const body = await response.json();
+	try {
+		const response = await fetch(url);
 
-	return body;
+		if (!response.ok) {
+			throw new Error(ERROR_MESSAGE);
+		}
+
+		const body = await response.json();
+		return body;
+	} catch (e) {
+		alert(e.message)
+	};
 };
