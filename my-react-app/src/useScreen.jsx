c7792e3eafb4abdb9852527601b8ecc90d2f33fd
Kirill108 <kirill108bol@gmail.com>
2022-12-26 18:14:00 +0100

    усбрал логи

--- my-react-app/src/useScreen.jsx
+++ my-react-app/src/useScreen.jsx
@@ -7,18 +7,14 @@ function useScreen() {
     const [isMobile, setIsMobile] = useState(false);
     const [isDesktop, setIsDesktop] = useState(false);
 
-	window.onload = function() {
-		if (screenWidth <= defaultWidthMobile) {
-			setIsMobile(true);
-		} else {
-			setIsDesktop(true);
-		}
-	}
-   
-    // console.log(' isDesktop: ', isDesktop);
-    // console.log('isMobile: ', isMobile);
-    // console.log('screenWidth: ', screenWidth);
-	const result =[screenWidth, isMobile, isDesktop]
+    window.onload = function () {
+        if (screenWidth <= defaultWidthMobile) {
+            setIsMobile(true);
+        } else {
+            setIsDesktop(true);
+        }
+    };
+    const result = [screenWidth, isMobile, isDesktop];
     return result;
 }
 
