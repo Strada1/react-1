24c02fbca6a0f434a9f17797ef637c9cebffd52d
Kirill108 <kirill108bol@gmail.com>
2023-01-01 19:04:42 +0100

    контекста нет, но код чуть улучшил

---
+++ my-react-app/src/components/DisplayDetails.jsx
@@ -0,0 +1,32 @@
+function DisplayDetalis(props) {
+    const {
+        city,
+        tab,
+        feelsLike,
+        weatherStatus,
+        sunrise,
+        sunset,
+        temperature,
+    } = props;
+    if (tab !== 'tab2') {
+        return null;
+    }
+    return (
+        <div className="tab-active" id="display-details">
+            <div className="city-name-details">{city}</div>
+            <div className="data-list">
+                <div className="temperature-details">
+                    Temperature: {temperature}°
+                </div>
+                <div className="feels-like-details">
+                    Feels like: {feelsLike}
+                </div>
+                <div className="weather-details">Weather: {weatherStatus}</div>
+                <div className="sunrise-details">Sunrise: {sunrise}</div>
+                <div className="sunset-details">Sunset: {sunset}</div>
+            </div>
+        </div>
+    );
+}
+
+export { DisplayDetalis };
