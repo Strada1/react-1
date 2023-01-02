5c87980aa003ca6ac28fe11d5aa483db9d1e1e54
yarmkhlv <yarmkhlv@gmail.com>
2023-01-02 17:03:48 +0400

    #15 Свой хук

--- my-react-app/src/App/App.jsx
+++ my-react-app/src/App/App.jsx
@@ -1,5 +1,20 @@
+/* eslint-disable no-nested-ternary */
+import React from 'react'
+import useScreen from '../customsHooks.js/useScreen'
+
 function App() {
-  return <div className="App" />
+  const { isMobile, isDesktop } = useScreen()
+  return (
+    <div className="App">
+      {isMobile ? (
+        <div>mobile or tablet</div>
+      ) : isDesktop ? (
+        <div>laptop or desktop</div>
+      ) : (
+        <div>undefined</div>
+      )}
+    </div>
+  )
 }
 
 export default App
